85456d1095224a78eb0f5c8591094f35e1def202
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-09-14 21:44:52 +0300

    1. Quartz [#175122 #201909]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -6,6 +6,7 @@ import org.quartz.impl.StdSchedulerFactory;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
@@ -14,45 +15,41 @@ import static org.quartz.SimpleScheduleBuilder.*;
 public class AlertRabbit {
     public static void main(String[] args) {
         try {
+            Properties properties = getProperties();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(getInterval())
+                    .withIntervalInSeconds(
+                            Integer.parseInt(properties.getProperty("rabbit.interval"))
+                    )
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
-            se.printStackTrace();
+        } catch (SchedulerException e) {
+            e.printStackTrace();
         }
     }
 
     public static class Rabbit implements Job {
         @Override
-        public void execute(JobExecutionContext context) throws JobExecutionException {
+        public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
         }
     }
 
-    public static int getInterval() {
-        int result = 0;
-        try (BufferedReader reader = new BufferedReader(
-                new FileReader(
-                        "C:\\projects\\job4j_grabber\\src\\main\\resources\\rabbit.properties"
-                )
+    public static Properties getProperties() {
+        Properties properties = new Properties();
+        try (BufferedReader in = new BufferedReader(
+                new FileReader(".\\src\\main\\resources\\rabbit.properties")
         )) {
-            String str = reader.readLine();
-            String[] subStr = str.split("=");
-            result = Integer.parseInt(subStr[1]);
+            properties.load(in);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if (result < 0) {
-            throw new IllegalArgumentException("Некорректный интервал запуска");
-        }
-        return result;
+        return properties;
     }
 }
